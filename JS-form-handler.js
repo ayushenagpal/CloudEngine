93d42420ef9135e5027a9eeb2592b791f2fae073
ayushenagpal <47947387+ayushenagpal@users.noreply.github.com>
2022-08-05 16:02:16 -0400

    Update JS-form-handler.js

--- JS-form-handler.js
+++ JS-form-handler.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
       $.ajax({
         type: 'POST',
-        url: 'https://nnzm1ri0uf.execute-api.us-east-1.amazonaws.com/v1/register',
+        url: 'https://74rsnsyvdg.execute-api.us-east-1.amazonaws.com/Beta',
         contentType: 'application/json',
         data: data,
         success: function(res) {
